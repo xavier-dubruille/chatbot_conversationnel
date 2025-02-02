6af0548ada066f2891284f3ec89de48eda0f7f27
Xavier Dubruille <x.dubruille@.ephec.be>
2025-02-02 23:11:25 +0100

    fix bad model naming (only for azure ?)

--- src/open_ai_stuff.py
+++ src/open_ai_stuff.py
@@ -11,9 +11,12 @@ openAiCli = apps.openAiCli
 async def cli(scenario_id, messages):
     # aka role  # todo: this section, along with the scenario_utils should be cleaned, dried and moved
     scenario_config = get_scenario_config(scenario_id)
+    role_model = "gpt-4o-mini" if scenario_config.role_model == "gpt4o-mini" \
+        else "gpt-4o" if scenario_config.role_model == "gpt4o" \
+        else scenario_config.role_model
     messages_to_send = [{"role": "system", "content": scenario_config.role_prompt}] + messages
     stream = await client.chat.completions.create(
-        model=scenario_config.role_model,
+        model=role_model,
         temperature=float(scenario_config.role_temperature),
         messages=messages_to_send,
         stream=True,
